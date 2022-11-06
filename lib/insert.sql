f2bd184b1397e7dfecae98932fc6fceec21806fd
Teana Zhong <Teanawk@gmail.com>
2022-11-06 13:45:19 -0500

    commit tree 01bd1cab59250d73a3708aeb3b51e227179b8491

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr. Chocolate", "M");
+INSERT INTO bears VALUES(2, "Rowdy", "M");
+INSERT INTO bears VALUES(3, "Tabitha", "M");
+INSERT INTO bears VALUES(4, "Sergeant Brown", "M");
+INSERT INTO bears VALUES(5, "Melissa", "M");
+INSERT INTO bears VALUES(6, "Grinch", "F");
+INSERT INTO bears VALUES(7, "Wendy", "F");
+INSERT INTO bears VALUES(8, null, "F");
